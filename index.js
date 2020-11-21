8b7eafa7327d063cb99ff2dc97965a635113f198
Jack <jckkempton@gmail.com>
2020-11-21 13:16:15 -0700

    added array of questions

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
+var inquirer = require('inquirer');
 // array of questions for user
-const questions = [
 
-];
+const questions = [{
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'What is the Description of your Project?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'How would a user install your Project?',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'How would someone use this application?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'How would someone contribute to this project?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'How would someone test this application?',
+        name: 'test'
+    }];
 
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
-
-}
-
+function init () {
+    inquirer
+    .prompt(questions)}
 // function call to initialize program
 init();
